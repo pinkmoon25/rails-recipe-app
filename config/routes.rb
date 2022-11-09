0f312b1b4a1fc5059a1bb852ed28d7069b566731
Tushar <singhtushar348@gmail.com>
2022-11-09 18:17:35 +0530

    Create routes for recipe_foods and public_recipes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   root "home#index"
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :recipes, except: :update
+  resources :recipes, except: :update do
+    resources :recipe_foods, except: :update
+  end
   resources :foods, except: :update
   resources :public_recipes, only: :index
 end
