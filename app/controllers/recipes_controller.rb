3b02acec1797139ba27d222441f996a4ddc29cd5
Tushar <singhtushar348@gmail.com>
2022-11-09 18:12:52 +0530

    Allow show action for public recipes

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,6 +1,6 @@
 class RecipesController < ApplicationController
-  before_action :authenticate_user!
-  
+  before_action :authenticate_user!, except: [:show]
+
   def index
     @recipes = current_user.recipes
   end
